bfd10a3dd3e4dcb1bedab75db40a68551fb7f974
LuisTrujano <134048513+LuisTrujano@users.noreply.github.com>
2025-02-03 16:22:47 -0600

    Update index.php

--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -1,1 +1,20 @@
 <h2 class="dashboard__heading"><?php echo $titulo; ?></h2>
+
+<body class="dashboard">
+        <?php 
+            include_once __DIR__ .'/templates/admin-header.php';
+        ?>
+        <div class="dashboard__grid">
+            <?php
+                include_once __DIR__ .'/templates/admin-sidebar.php';  
+            ?>
+
+            <main class="dashboard__contenido">
+                <?php 
+                    echo $contenido; 
+                ?> 
+            </main>
+        </div>
+
+    <script src="/build/js/bundle.min.js" defer></script>
+</body>
